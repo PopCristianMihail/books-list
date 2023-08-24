3061ce39bfa6668e2883ffb1fb1f1ac2e7727502
Cristian <popcristianmihail@gmail.com>
2023-08-24 14:58:47 +0300

    created the BookCreate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App () {
 
     const [books,setBooks] = useState([]);
 
-    return <div>App</div>
+    const CreateBook = (title) => {
+        console.log('Need to add book with the title:',title);
+    }
+
+    return <div>
+        <BookCreate onCreate={CreateBook}/>
+    </div>
 }
 
 export default App;
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 
-function BookCreate() {
+function BookCreate({ onCreate }) {
+  
+  const [title,setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+  }
+  
   return (
-    <div>BookCreate</div>
+    <div className="book-create">
+    <h3>Add a Book!</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" type="text" value={title} onChange={handleChange}/>
+        <button className="button">Create!</button>
+      </form>
+    </div>
   )
 }
 
-export default BookCreate
+export default BookCreate;
